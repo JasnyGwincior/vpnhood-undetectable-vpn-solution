fc6628e1c9b970e9694543d27bed64d0980bb1b8
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:12:10 +0200

    Add Tests/VpnHood.Test/Providers/TestTracker.cs 1752696680344

---
+++ Tests/VpnHood.Test/Providers/TestTracker.cs
@@ -0,0 +1,27 @@
+﻿using Ga4.Trackers;
+
+namespace VpnHood.Test.Providers;
+
+public class TestTracker : ITracker
+{
+    public bool IsEnabled { get; set; }
+    public List<TrackEvent> TrackEvents { get; } = [];
+
+    public Task Track(IEnumerable<TrackEvent> trackEvents, CancellationToken cancellationToken)
+    {
+        return Task.WhenAll(trackEvents.Select(x=>Track(x, cancellationToken)));
+    }
+
+    public Task Track(TrackEvent trackEvent, CancellationToken cancellationToken)
+    {
+        TrackEvents.Add(trackEvent);
+        return Task.CompletedTask;
+    }
+
+    public TrackEvent? FindEvent(string eventName, string parameterName, object parameterValue)
+    {
+        return TrackEvents.FirstOrDefault(e =>
+            e.EventName == eventName &&
+            e.Parameters.ContainsKey(parameterName) && e.Parameters[parameterName].Equals(parameterValue));
+    }
+}
