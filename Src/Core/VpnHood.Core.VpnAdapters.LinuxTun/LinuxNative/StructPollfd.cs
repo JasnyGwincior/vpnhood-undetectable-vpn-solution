10cf5b8b1bdd3c62256ca0cbd5b25e105347ca9a
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:17:37 +0200

    Add Src/Core/VpnHood.Core.VpnAdapters.LinuxTun/LinuxNative/StructPollfd.cs 1752697007339

---
+++ Src/Core/VpnHood.Core.VpnAdapters.LinuxTun/LinuxNative/StructPollfd.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace VpnHood.Core.VpnAdapters.LinuxTun.LinuxNative;
+
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+internal struct StructPollfd
+{
+    public int Fd;
+    public short Events;
+    public short Revents;
+}
