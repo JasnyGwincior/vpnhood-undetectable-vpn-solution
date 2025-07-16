15ea0cd8efaefb895c0c26334ad7ecf714859a82
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:42:20 +0200

    Add Src/Core/VpnHood.Core.Client.Device.Android/AndroidUiContext.cs 1752698490089

---
+++ Src/Core/VpnHood.Core.Client.Device.Android/AndroidUiContext.cs
@@ -0,0 +1,21 @@
+﻿using VpnHood.Core.Client.Device.Droid.ActivityEvents;
+using VpnHood.Core.Client.Device.UiContexts;
+
+namespace VpnHood.Core.Client.Device.Droid;
+
+public class AndroidUiContext(IActivityEvent activityEvent) : IUiContext
+{
+    public IActivityEvent ActivityEvent => activityEvent;
+    public Activity Activity => activityEvent.Activity;
+
+    public bool IsActive {
+        get {
+            try {
+                return activityEvent.Activity.Window?.DecorView.RootView?.IsShown == true;
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+    }
+}
