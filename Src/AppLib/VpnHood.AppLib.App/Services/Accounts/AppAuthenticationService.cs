389d156a03afc4dca084fddcb612c9a040d81694
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:49 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Services/Accounts/AppAuthenticationService.cs 1752698579411

---
+++ Src/AppLib/VpnHood.AppLib.App/Services/Accounts/AppAuthenticationService.cs
@@ -0,0 +1,32 @@
+﻿using VpnHood.AppLib.Abstractions;
+using VpnHood.Core.Client.Device.UiContexts;
+using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.AppLib.Services.Accounts;
+
+public class AppAuthenticationService(
+    AppAccountService accountService,
+    IAppAuthenticationProvider accountProvider)
+    : IDisposable
+{
+    public bool IsSignInWithGoogleSupported => accountProvider.IsSignInWithGoogleSupported;
+    public string? UserId => accountProvider.UserId;
+    public HttpClient HttpClient => accountProvider.HttpClient;
+
+    public async Task SignInWithGoogle(IUiContext uiContext)
+    {
+        await accountProvider.SignInWithGoogle(uiContext).Vhc();
+        await accountService.Refresh(updateCurrentClientProfile: true).Vhc();
+    }
+
+    public async Task SignOut(IUiContext uiContext)
+    {
+        await accountProvider.SignOut(uiContext).Vhc();
+        await accountService.Refresh(updateCurrentClientProfile: true).Vhc();
+    }
+
+    public void Dispose()
+    {
+        accountProvider.Dispose();
+    }
+}
