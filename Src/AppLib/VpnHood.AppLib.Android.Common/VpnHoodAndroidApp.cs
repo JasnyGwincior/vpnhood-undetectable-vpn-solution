1af4104982c74de28fd1d881114891412737ea15
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:44:32 +0200

    Add Src/AppLib/VpnHood.AppLib.Android.Common/VpnHoodAndroidApp.cs 1752698622229

---
+++ Src/AppLib/VpnHood.AppLib.Android.Common/VpnHoodAndroidApp.cs
@@ -0,0 +1,39 @@
+﻿using Android.Runtime;
+using Ga4.Trackers;
+using VpnHood.Core.Client.Device.Droid;
+using VpnHood.Core.Client.Device.Droid.Utils;
+
+namespace VpnHood.AppLib.Droid.Common;
+
+public abstract class VpnHoodAndroidApp(IntPtr javaReference, JniHandleOwnership transfer)
+    : Application(javaReference, transfer)
+{
+    protected abstract AppOptions CreateAppOptions();
+    protected virtual Type GetVpnServiceType() => typeof(ITracker);
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+
+        //app init
+        if (!VpnHoodApp.IsInit && !AndroidDevice.IsVpnServiceProcess) {
+            var options = CreateAppOptions();
+            options.UiProvider ??= new AndroidUiProvider();
+            options.CultureProvider ??= AndroidAppCultureProvider.CreateIfSupported();
+            options.DeviceId ??= AndroidUtil.GetDeviceId(this); //this will be hashed using AppId
+
+            var vpnHoodDevice = AndroidDevice.Create();
+            VpnHoodApp.Init(vpnHoodDevice, options);
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing) {
+            if (VpnHoodApp.IsInit) 
+                VpnHoodApp.Instance.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+}
