a1e7afb131f294a9808a63e2be6105d6a7d5b021
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:12:20 +0200

    Add Tests/VpnHood.Test/Device/TestVpnAdapter.cs 1752696690307

---
+++ Tests/VpnHood.Test/Device/TestVpnAdapter.cs
@@ -0,0 +1,15 @@
+﻿using VpnHood.Core.VpnAdapters.WinDivert;
+
+namespace VpnHood.Test.Device;
+
+public class TestVpnAdapter(TestVpnAdapterOptions vpnAdapterOptions)
+    : WinDivertVpnAdapter(new WinDivertVpnAdapterSettings {
+        AdapterName = "VpnHoodTestAdapter",
+        Blocking = false,
+        AutoDisposePackets = true,
+        SimulateDns = vpnAdapterOptions.SimulateDns
+    })
+{
+    public TestVpnAdapterOptions VpnAdapterOptions { get; } = vpnAdapterOptions;
+
+}
