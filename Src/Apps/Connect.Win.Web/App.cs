6ed7913722f526a49c00300d1235297baa1d247c
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:46:40 +0200

    Add Src/Apps/Connect.Win.Web/App.cs 1752698750879

---
+++ Src/Apps/Connect.Win.Web/App.cs
@@ -0,0 +1,43 @@
+﻿using VpnHood.App.Client;
+using VpnHood.AppLib;
+using VpnHood.AppLib.Win.Common;
+using VpnHood.AppLib.Win.Common.WpfSpa;
+
+namespace VpnHood.App.Connect.Win.Web;
+
+public class App : VpnHoodWpfSpaApp
+{
+    [STAThread]
+    public static void Main()
+    {
+        var app = new App();
+        app.Run();
+    }
+
+    public override bool SpaListenToAllIps => AppConfigs.Instance.SpaListenToAllIps;
+    public override int? SpaDefaultPort => AppConfigs.Instance.SpaDefaultPort;
+
+    protected override AppOptions CreateAppOptions()
+    {
+        var appConfigs = AppConfigs.Load();
+
+        // load app settings and resources
+        var resources = ConnectAppResources.Resources;
+        resources.Strings.AppName = AppConfigs.AppName;
+
+        return new AppOptions("com.vpnhood.connect.windows", "VpnHoodConnect", AppConfigs.IsDebugMode) {
+            UiName = "VpnHoodConnect",
+            Resources = resources,
+            AccessKeys = [appConfigs.DefaultAccessKey],
+            UpdateInfoUrl = appConfigs.UpdateInfoUrl,
+            UpdaterProvider = new AdvancedInstallerUpdaterProvider(),
+            IsAddAccessKeySupported = false,
+            LocalSpaHostName = "my-vpnhood-connect",
+            AllowEndPointTracker = appConfigs.AllowEndPointTracker,
+            Ga4MeasurementId = appConfigs.Ga4MeasurementId,
+            LogServiceOptions = {
+                SingleLineConsole = false
+            }
+        };
+    }
+}
