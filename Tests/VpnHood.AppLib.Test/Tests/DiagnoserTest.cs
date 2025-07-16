10ed1ba8e05584cdbc79b55e840cb44adc879dd6
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:45:29 +0200

    Add Tests/VpnHood.AppLib.Test/Tests/DiagnoserTest.cs 1752698679633

---
+++ Tests/VpnHood.AppLib.Test/Tests/DiagnoserTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VpnHood.Core.Client.Abstractions.Exceptions;
+using VpnHood.Test;
+
+namespace VpnHood.AppLib.Test.Tests;
+
+[TestClass]
+public class DiagnoserTest : TestAppBase
+{
+    [TestMethod]
+    public async Task NormalConnect_NoInternet()
+    {
+        // create server
+        await using var server = await TestHelper.CreateServer();
+        var token = TestHelper.CreateAccessToken(server);
+        token.ServerToken.HostEndPoints = [TestConstants.InvalidEp];
+
+        // create client
+        var appOptions = TestAppHelper.CreateAppOptions();
+        appOptions.AutoDiagnose = true;
+        appOptions.ConnectTimeout = TimeSpan.FromSeconds(30);
+        await using var clientApp = TestAppHelper.CreateClientApp(appOptions: appOptions);
+        var clientProfile = clientApp.ClientProfileService.ImportAccessKey(token.ToAccessKey());
+
+        // ************
+        // NoInternetException
+        clientApp.Diagnoser.TestHttpUris = [TestConstants.InvalidUri];
+        clientApp.Diagnoser.TestNsIpEndPoints = [TestConstants.InvalidEp];
+        clientApp.Diagnoser.TestPingIpAddresses = [TestConstants.InvalidIp];
+        await Assert.ThrowsExceptionAsync<NoInternetException>(() =>
+            clientApp.Connect(clientProfile.ClientProfileId));
+    }
+}
