fdc017564b7c5fdce127ad755ed5bb91c2ba9c44
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:45:18 +0200

    Add Src/AppLib/VpnHood.AppLib.Abstractions/BillingPurchaseState.cs 1752698668563

---
+++ Src/AppLib/VpnHood.AppLib.Abstractions/BillingPurchaseState.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.AppLib.Abstractions;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum BillingPurchaseState
+{
+    None = 0,
+    Started = 1,
+    Processing = 2
+}
