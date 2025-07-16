12c9ffe5d43c6978954df7892576e575fd2813bb
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:45:20 +0200

    Add Src/AppLib/VpnHood.AppLib.Abstractions/AppAdType.cs 1752698670455

---
+++ Src/AppLib/VpnHood.AppLib.Abstractions/AppAdType.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.AppLib.Abstractions;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AppAdType
+{
+    InterstitialAd,
+    RewardedAd,
+    AppOpenAd
+}
