8c8bff1ee7573cddd0705d3fdbbaae457796a910
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:38:42 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Channels/IChannel.cs 1752698271850

---
+++ Src/Core/VpnHood.Core.Tunneling/Channels/IChannel.cs
@@ -0,0 +1,12 @@
+﻿using VpnHood.Core.Common.Messaging;
+
+namespace VpnHood.Core.Tunneling.Channels;
+
+public interface IChannel : IDisposable
+{
+    string ChannelId { get; }
+    DateTime LastActivityTime { get; }
+    Traffic Traffic { get; }
+    void Start();
+    PacketChannelState State { get; }
+}
