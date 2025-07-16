18989bb3b896d2a21d66230a0f34939efaffbf90
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:38:25 +0200

    Add Src/Core/VpnHood.Core.Server/Utils/ClientStreamExtensions.cs 1752698255122

---
+++ Src/Core/VpnHood.Core.Server/Utils/ClientStreamExtensions.cs
@@ -0,0 +1,30 @@
+﻿using VpnHood.Core.Common.Messaging;
+using VpnHood.Core.Toolkit.Utils;
+using VpnHood.Core.Tunneling.ClientStreams;
+
+namespace VpnHood.Core.Server.Utils;
+
+public static class ClientStreamExtensions
+{
+    public static async Task WriteResponseAsync(this IClientStream clientStream, SessionResponse sessionResponse,
+        CancellationToken cancellationToken)
+    {
+        var responseData = StreamUtils.ObjectToJsonBuffer(sessionResponse);
+
+        // If the client stream requires an HTTP response, write it to the client stream
+        if (clientStream.RequireHttpResponse) {
+            clientStream.RequireHttpResponse = false;
+            await clientStream.Stream.WriteAsync(HttpResponseBuilder.Ok(responseData.Length), cancellationToken).Vhc();
+        }
+
+        // Write the session response to the client stream
+        await clientStream.Stream.WriteAsync(responseData, cancellationToken).Vhc();
+    }
+
+    public static async Task DisposeAsync(this IClientStream clientStream, SessionResponse sessionResponse, CancellationToken cancellationToken)
+    {
+        // Write the session response to the client stream
+        await clientStream.WriteResponseAsync(sessionResponse, cancellationToken).Vhc();
+        clientStream.Dispose();
+    }
+}
