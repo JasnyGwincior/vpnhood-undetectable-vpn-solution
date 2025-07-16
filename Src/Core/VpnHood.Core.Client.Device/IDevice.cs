d2b196b46156e9f8ce76693686485fc9dbf31be3
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:30 +0200

    Add Src/Core/VpnHood.Core.Client.Device/IDevice.cs 1752698440923

---
+++ Src/Core/VpnHood.Core.Client.Device/IDevice.cs
@@ -0,0 +1,19 @@
+﻿using VpnHood.Core.Client.Device.UiContexts;
+
+namespace VpnHood.Core.Client.Device;
+
+public interface IDevice : IDisposable
+{
+    string VpnServiceConfigFolder { get; }
+    bool IsTv { get; }
+    bool IsExcludeAppsSupported { get; }
+    bool IsIncludeAppsSupported { get; }
+    bool IsAlwaysOnSupported { get; }
+    bool IsBindProcessToVpnSupported { get; }
+    string OsInfo { get; }
+    DeviceMemInfo? MemInfo { get; }
+    DeviceAppInfo[] InstalledApps { get; }
+    Task RequestVpnService(IUiContext? uiContext, TimeSpan timeout, CancellationToken cancellationToken);
+    Task StartVpnService(CancellationToken cancellationToken);
+    void BindProcessToVpn(bool value);
+}
