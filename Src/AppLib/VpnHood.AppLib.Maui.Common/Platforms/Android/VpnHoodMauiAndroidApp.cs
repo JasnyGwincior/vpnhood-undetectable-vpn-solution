fb8ea701e1b7f6def2c0309a9f14b98bb828c381
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:18 +0200

    Add Src/AppLib/VpnHood.AppLib.Maui.Common/Platforms/Android/VpnHoodMauiAndroidApp.cs 1752698548907

---
+++ Src/AppLib/VpnHood.AppLib.Maui.Common/Platforms/Android/VpnHoodMauiAndroidApp.cs
@@ -0,0 +1,15 @@
+﻿using VpnHood.AppLib.Droid.Common;
+using VpnHood.Core.Client.Device.Droid;
+
+// ReSharper disable once CheckNamespace
+namespace VpnHood.AppLib.Maui.Common;
+
+internal class VpnHoodMauiAndroidApp : IVpnHoodMauiApp
+{
+    public VpnHoodApp Init(AppOptions options)
+    {
+        var device = AndroidDevice.Create();
+        options.CultureProvider ??= AndroidAppCultureProvider.CreateIfSupported();
+        return VpnHoodApp.Init(device, options);
+    }
+}
