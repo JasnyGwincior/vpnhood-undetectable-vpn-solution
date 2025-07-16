a39d2de0c093c90598b3eaa8b2fcf1d55858845d
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:42:54 +0200

    Add Src/AppLib/VpnHood.AppLib.WebServer/Controllers/BillingController.cs 1752698524098

---
+++ Src/AppLib/VpnHood.AppLib.WebServer/Controllers/BillingController.cs
@@ -0,0 +1,34 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using VpnHood.AppLib.Abstractions;
+using VpnHood.AppLib.Services.Accounts;
+using VpnHood.AppLib.WebServer.Api;
+using VpnHood.Core.Client.Device.UiContexts;
+
+namespace VpnHood.AppLib.WebServer.Controllers;
+
+internal class BillingController : WebApiController, IBillingController
+{
+    private static AppBillingService BillingService => VpnHoodApp.Instance.Services.AccountService?.BillingService
+                                                       ?? throw new Exception(
+                                                           "Billing service is not available at this moment.");
+
+    [Route(HttpVerbs.Get, "/subscription-plans")]
+    public Task<SubscriptionPlan[]> GetSubscriptionPlans()
+    {
+        return BillingService.GetSubscriptionPlans();
+    }
+
+    [Route(HttpVerbs.Post, "/purchase")]
+    public Task<string> Purchase([QueryField] string planId)
+    {
+        return BillingService.Purchase(AppUiContext.RequiredContext, planId);
+    }
+
+    [Route(HttpVerbs.Get, "/purchase-options")]
+    public Task<AppPurchaseOptions> GetPurchaseOptions()
+    {
+        return VpnHoodApp.Instance.GetPurchaseOptions();
+    }
+}
