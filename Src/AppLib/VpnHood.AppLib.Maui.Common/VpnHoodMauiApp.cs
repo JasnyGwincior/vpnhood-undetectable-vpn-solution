e4fee148474d8f35997335cd71effff3ece6addd
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:13 +0200

    Add Src/AppLib/VpnHood.AppLib.Maui.Common/VpnHoodMauiApp.cs 1752698543089

---
+++ Src/AppLib/VpnHood.AppLib.Maui.Common/VpnHoodMauiApp.cs
@@ -0,0 +1,24 @@
+﻿using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.AppLib.Maui.Common;
+
+public class VpnHoodMauiApp : Singleton<VpnHoodMauiApp>
+{
+    public static VpnHoodApp Init(AppOptions options)
+    {
+        var app = CreateApp();
+        return app.Init(options);
+    }
+
+    private static IVpnHoodMauiApp CreateApp()
+    {
+#if ANDROID
+        return new VpnHoodMauiAndroidApp();
+
+#elif WINDOWS
+        return new VpnHoodMauiWinUiApp();
+#else
+        throw new PlatformNotSupportedException();
+#endif
+    }
+}
