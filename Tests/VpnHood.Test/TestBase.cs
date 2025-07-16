f274fc5853ffacd985343bdc471d41b8a1b73ce5
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:12:06 +0200

    Add Tests/VpnHood.Test/TestBase.cs 1752696676358

---
+++ Tests/VpnHood.Test/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VpnHood.Core.Toolkit.Logging;
+using VpnHood.Core.Tunneling;
+
+namespace VpnHood.Test;
+
+public abstract class TestBase
+{
+    protected TestHelper TestHelper { get; private set; } = null!;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        TestHelper = CreateTestHelper();
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+        TestHelper.Dispose();
+    }
+
+    protected void Log(string message)
+    {
+        VhLogger.Instance.LogInformation(GeneralEventId.Test, message);
+    }
+
+    protected virtual TestHelper CreateTestHelper() => new();
+}
