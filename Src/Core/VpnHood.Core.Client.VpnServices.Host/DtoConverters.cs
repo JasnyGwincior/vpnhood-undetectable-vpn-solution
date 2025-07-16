c03d6f0109e8b20bbe05c6e5d922eabfbb61b2d6
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:13 +0200

    Add Src/Core/VpnHood.Core.Client.VpnServices.Host/DtoConverters.cs 1752698423396

---
+++ Src/Core/VpnHood.Core.Client.VpnServices.Host/DtoConverters.cs
@@ -0,0 +1,43 @@
+﻿using VpnHood.Core.Client.ConnectorServices;
+using VpnHood.Core.Client.VpnServices.Abstractions;
+
+namespace VpnHood.Core.Client.VpnServices.Host;
+
+internal static class DtoConverters
+{
+    public static SessionStatus ToDto(this ISessionStatus sessionStatus)
+    {
+        var ret = new SessionStatus {
+            ConnectorStat = sessionStatus.ConnectorStat.ToDto(),
+            ActiveClientCount = sessionStatus.ActiveClientCount,
+            CanExtendByRewardedAd = sessionStatus.CanExtendByRewardedAd,
+            CycleTraffic = sessionStatus.CycleTraffic,
+            PacketChannelCount = sessionStatus.ActivePacketChannelCount,
+            IsUdpMode = sessionStatus.IsUdpMode,
+            SessionExpirationTime = sessionStatus.SessionExpirationTime,
+            SessionMaxTraffic = sessionStatus.SessionMaxTraffic,
+            SessionTraffic = sessionStatus.SessionTraffic,
+            SessionSplitTraffic = sessionStatus.SessionSplitTraffic,
+            TotalTraffic = sessionStatus.TotalTraffic,
+            Speed = sessionStatus.Speed,
+            TcpPassthruCount = sessionStatus.TcpPassthruCount,
+            TcpTunnelledCount = sessionStatus.TcpTunnelledCount,
+            AdRequest = sessionStatus.AdRequest
+        };
+
+        return ret;
+    }
+
+    public static ConnectorStat ToDto(this ClientConnectorStat connectorStat)
+    {
+        var dto = new ConnectorStat {
+            CreatedConnectionCount = connectorStat.CreatedConnectionCount,
+            FreeConnectionCount = connectorStat.FreeConnectionCount,
+            RequestCount = connectorStat.RequestCount,
+            ReusedConnectionFailedCount = connectorStat.ReusedConnectionFailedCount,
+            ReusedConnectionSucceededCount = connectorStat.ReusedConnectionSucceededCount
+        };
+
+        return dto;
+    }
+}
