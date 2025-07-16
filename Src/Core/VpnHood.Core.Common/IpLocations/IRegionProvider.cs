aa5e8f3540f79d608c33221c1af87738a82b7b88
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:02 +0200

    Add Src/Core/VpnHood.Core.Common/IpLocations/IRegionProvider.cs 1752698412596

---
+++ Src/Core/VpnHood.Core.Common/IpLocations/IRegionProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace VpnHood.Core.Common.IpLocations;
+
+public interface IRegionProvider
+{
+    string GetClientCountryCode(bool allowVpnServer);
+    Task<string> GetClientCountryCodeAsync(bool allowVpnServer, CancellationToken cancellationToken);
+}
