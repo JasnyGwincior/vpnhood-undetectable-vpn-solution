63380cfdedd6433b1f60268cf7052ab9dbb2ed7f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:47:05 +0200

    Add Src/Apps/Client.Win.Web/App.cs 1752698775141

---
+++ Src/Apps/Client.Win.Web/App.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Principal;
+using VpnHood.AppLib;
+using VpnHood.AppLib.Win.Common;
+using VpnHood.AppLib.Win.Common.WpfSpa;
+
+namespace VpnHood.App.Client.Win.Web;
+
+public class App : VpnHoodWpfSpaApp
+{
+    [STAThread]
+    public static void Main()
+    {
+        var app = new App();
+        app.Run();
+    }
+
+    public override bool SpaListenToAllIps => AppConfigs.Instance.SpaListenToAllIps;
+    public override int? SpaDefaultPort => AppConfigs.Instance.SpaDefaultPort;
+
+    protected override AppOptions CreateAppOptions()
+    {
+        var appConfigs = AppConfigs.Load();
+
+        var resources = ClientAppResources.Resources;
+        resources.Strings.AppName = AppConfigs.AppName;
+
+        return new AppOptions(appConfigs.AppId, appConfigs.StorageFolderName, AppConfigs.IsDebugMode) {
+            DeviceId = WindowsIdentity.GetCurrent().User?.Value,
+            Resources = resources,
+            AccessKeys = AppConfigs.IsDebug ? [appConfigs.DefaultAccessKey] : [],
+            UpdateInfoUrl = appConfigs.UpdateInfoUrl,
+            UpdaterProvider = new AdvancedInstallerUpdaterProvider(),
+            IsAddAccessKeySupported = true,
+            IsLocalNetworkSupported = true,
+            LocalSpaHostName = "my-vpnhood",
+            LogServiceOptions = {
+                SingleLineConsole = false
+            }
+        };
+    }
+}
