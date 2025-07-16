9f5097d8783188ef30e1ac2efdae9a2b38c060ca
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:05 +0200

    Add Src/Core/VpnHood.Core.Common/Exceptions/ShowAdNoUiException.cs 1752698415558

---
+++ Src/Core/VpnHood.Core.Common/Exceptions/ShowAdNoUiException.cs
@@ -0,0 +1,22 @@
+﻿namespace VpnHood.Core.Common.Exceptions;
+
+public class ShowAdNoUiException : ShowAdException
+{
+    private const string Msg = "Could not show any ad because the app window was not open.";
+
+    public ShowAdNoUiException() : base(Msg)
+    {
+    }
+
+    public ShowAdNoUiException(Exception innerException) : base(Msg, innerException)
+    {
+    }
+
+    public ShowAdNoUiException(string msg) : base(msg)
+    {
+    }
+
+    public ShowAdNoUiException(string msg, Exception innerException) : base(msg, innerException)
+    {
+    }
+}
