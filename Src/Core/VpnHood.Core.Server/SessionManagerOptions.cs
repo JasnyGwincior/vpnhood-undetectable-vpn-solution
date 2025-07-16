ae13d1dbe2fc691246d0b7a80e738beb3246180d
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:38:12 +0200

    Add Src/Core/VpnHood.Core.Server/SessionManagerOptions.cs 1752698242677

---
+++ Src/Core/VpnHood.Core.Server/SessionManagerOptions.cs
@@ -0,0 +1,11 @@
+﻿using VpnHood.Core.Toolkit.Net;
+
+namespace VpnHood.Core.Server;
+
+internal class SessionManagerOptions
+{
+    public required TimeSpan DeadSessionTimeout { get; init; }
+    public required TimeSpan HeartbeatInterval { get; init; }
+    public required IpNetwork VirtualIpNetworkV4 { get; init; }
+    public required IpNetwork VirtualIpNetworkV6 { get; init; }
+}
