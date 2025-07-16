9c48e973df3a56925301a63aaf85cad7c7eb47ea
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:48 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Services/Ads/AppAdOptions.cs 1752698578413

---
+++ Src/AppLib/VpnHood.AppLib.App/Services/Ads/AppAdOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.AppLib.Services.Ads;
+
+public class AppAdOptions
+{
+    public TimeSpan ShowAdPostDelay { get; set; } = TimeSpan.FromSeconds(3);
+    public TimeSpan LoadAdPostDelay { get; set; } = TimeSpan.FromSeconds(1);
+    public TimeSpan LoadAdTimeout { get; set; } = Debugger.IsAttached 
+        ? VhUtils.DebuggerTimeout : TimeSpan.FromSeconds(90);
+    public bool PreloadAd { get; set; }
+}
