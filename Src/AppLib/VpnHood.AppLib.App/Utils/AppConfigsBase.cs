ee1cb5a29dace948433377ce166277177d843b90
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:17 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Utils/AppConfigsBase.cs 1752698546981

---
+++ Src/AppLib/VpnHood.AppLib.App/Utils/AppConfigsBase.cs
@@ -0,0 +1,13 @@
+﻿using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.AppLib.Utils;
+
+public class AppConfigsBase<T> : Singleton<T> where T : Singleton<T>
+{
+    protected void Merge(string configName)
+    {
+        var json = VhUtils.GetAssemblyMetadata(typeof(T).Assembly, configName, "");
+        if (!string.IsNullOrEmpty(json))
+            JsonSerializerExt.PopulateObject(this, json, typeof(T));
+    }
+}
