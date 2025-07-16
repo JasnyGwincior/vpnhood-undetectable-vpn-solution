1a986fa90713d1fa38cce198d3e8bf124f5b78b8
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:37:31 +0200

    Add Src/Core/VpnHood.Core.Tunneling/ClientStreams/IClientStream.cs 1752698201953

---
+++ Src/Core/VpnHood.Core.Tunneling/ClientStreams/IClientStream.cs
@@ -0,0 +1,13 @@
+﻿using VpnHood.Core.Toolkit.Net;
+
+namespace VpnHood.Core.Tunneling.ClientStreams;
+
+public interface IClientStream : IDisposable
+{
+    string ClientStreamId { get; set; }
+    bool RequireHttpResponse { get; set; }
+    IPEndPointPair IpEndPointPair { get; }
+    Stream Stream { get; }
+    bool Connected { get; }
+    void PreventReuse();
+}
