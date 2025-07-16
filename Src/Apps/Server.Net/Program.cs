d76c163363c8f5e6c2f8993c888a78bc929cb1c7
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:45:37 +0200

    Add Src/Apps/Server.Net/Program.cs 1752698687613

---
+++ Src/Apps/Server.Net/Program.cs
@@ -0,0 +1,17 @@
+﻿using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.App.Server;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try {
+            using var serverApp = new ServerApp();
+            await serverApp.Start(args).Vhc();
+        }
+        catch (Exception ex) {
+            throw new Exception(ex.Message);
+        }
+    }
+}
