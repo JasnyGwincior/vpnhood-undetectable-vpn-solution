67b25e162eca71358dd06219609a9b5082e97691
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:45 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Settings/AppSettings.cs 1752698575513

---
+++ Src/AppLib/VpnHood.AppLib.App/Settings/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using VpnHood.Core.Common.IpLocations;
+
+namespace VpnHood.AppLib.Settings;
+
+public class AppSettings
+{
+    public int Version { get; set; } = 2;
+    public string ClientId { get; set; } = Guid.NewGuid().ToString();
+    public bool? IsQuickLaunchEnabled { get; set; }
+    public bool? IsNotificationEnabled { get; set; }
+    public bool IsStartupTrackerSent { get; set; }
+    public IpLocation? ClientIpLocation { get; set; }
+    public IpLocation? ClientIpLocationByServer { get; set; }
+    public DateTime ConfigTime { get; set; } = DateTime.Now;
+    public UserSettings UserSettings { get; set; } = new();
+
+    internal AppSettingsService? AppSettingsService { get; set; }
+
+    public void Save()
+    {
+        if (AppSettingsService == null)
+            throw new InvalidOperationException("AppSettingsService is not set");
+
+        AppSettingsService.Save();
+    }
+}
