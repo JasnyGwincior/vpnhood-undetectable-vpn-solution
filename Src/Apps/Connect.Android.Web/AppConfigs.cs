cbfe90d118b1dca76a977e448836e318af92b445
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:46:47 +0200

    Add Src/Apps/Connect.Android.Web/AppConfigs.cs 1752698757158

---
+++ Src/Apps/Connect.Android.Web/AppConfigs.cs
@@ -0,0 +1,39 @@
+using VpnHood.AppLib.Utils;
+using VpnHood.Core.Client.Abstractions;
+
+// ReSharper disable StringLiteralTypo
+// ReSharper disable CommentTypo
+// ReSharper disable HeuristicUnreachableCode
+namespace VpnHood.App.Connect.Droid.Web;
+
+internal class AppConfigs : AppConfigsBase<AppConfigs>
+{
+    public const string AppName = IsDebugMode ? "VpnHOOD! CONNECT (DEBUG)" : "VpnHood! CONNECT";
+
+    public string? UpdateInfoUrl { get; set; } =
+        "https://github.com/vpnhood/VpnHood.App.Connect/releases/latest/download/VpnHoodConnect-Android-web.json";
+
+    public int? SpaDefaultPort { get; set; } = IsDebugMode ? 9571 : 9570;
+    public bool SpaListenToAllIps { get; set; } = IsDebugMode;
+    public bool AllowEndPointTracker { get; set; } = true;
+    public string? Ga4MeasurementId { get; set; }
+    public string? AppsFlyerDevKey { get; set; }
+
+    // SampleAccessKey is a test access key, you should replace it with your own access key.
+    // It is limited and can not be used in production.
+    public string DefaultAccessKey { get; set; } = ClientOptions.SampleAccessKey;
+
+    public static AppConfigs Load()
+    {
+        var appConfigs = new AppConfigs();
+        appConfigs.Merge("AppSettings");
+        appConfigs.Merge("AppSettings_Environment");
+        return appConfigs;
+    }
+
+#if DEBUG
+    public const bool IsDebugMode = true;
+#else
+    public const bool IsDebugMode = false;
+#endif
+}
