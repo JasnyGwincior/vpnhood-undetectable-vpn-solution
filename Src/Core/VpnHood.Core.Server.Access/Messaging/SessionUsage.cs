b90c9e5f0836acf8b6110fe44db8026fc38fc0b8
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:39:02 +0200

    Add Src/Core/VpnHood.Core.Server.Access/Messaging/SessionUsage.cs 1752698292513

---
+++ Src/Core/VpnHood.Core.Server.Access/Messaging/SessionUsage.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using VpnHood.Core.Common.Messaging;
+
+namespace VpnHood.Core.Server.Access.Messaging;
+
+public class SessionUsage
+{
+    public required ulong SessionId { get; set; }
+
+    [Obsolete("Use ErrorCode")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool Closed { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public SessionErrorCode ErrorCode { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public long Sent { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public long Received { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? AdData { get; set; }
+    public Traffic ToTraffic() => new(Sent, Received);
+}
