65e8ad5cdb9caef20ba169926234f7514e750c2c
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:42:59 +0200

    Add Src/AppLib/VpnHood.AppLib.WebServer/Api/IpFilters.cs 1752698529544

---
+++ Src/AppLib/VpnHood.AppLib.WebServer/Api/IpFilters.cs
@@ -0,0 +1,10 @@
+﻿
+namespace VpnHood.AppLib.WebServer.Api;
+
+public class IpFilters
+{
+    public required string AdapterIpFilterIncludes { get; set; }
+    public required string AdapterIpFilterExcludes { get; set; }
+    public required string AppIpFilterIncludes { get; set; }
+    public required string AppIpFilterExcludes { get; set; }
+}
