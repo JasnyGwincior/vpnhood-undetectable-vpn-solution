b02a0cd9eb6ce55df6370e1f2c0b292cd8189750
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:12:22 +0200

    Add Tests/VpnHood.Test/Device/TestDeviceSocketFactory.cs 1752696692175

---
+++ Tests/VpnHood.Test/Device/TestDeviceSocketFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using VpnHood.Core.Tunneling.Sockets;
+using VpnHood.Test.Providers;
+
+namespace VpnHood.Test.Device;
+
+public class TestDeviceSocketFactory(TestDevice testDevice) : ISocketFactory
+{
+    private readonly TestSocketFactory _socketFactory = new();
+    public TcpClient CreateTcpClient(IPEndPoint ipEndPoint)
+    {
+        var tcpClient = new TcpClient(ipEndPoint.AddressFamily);
+        if (testDevice.VpnService?.CurrentVpnAdapter?.CanProtectSocket == true)
+            testDevice.VpnService?.CurrentVpnAdapter.ProtectSocket(tcpClient.Client);
+
+        return tcpClient;
+    }
+
+    public UdpClient CreateUdpClient(AddressFamily addressFamily)
+    {
+        var udpClient = new UdpClient(addressFamily);
+        if (testDevice.VpnService?.CurrentVpnAdapter?.CanProtectSocket == true)
+            testDevice.VpnService?.CurrentVpnAdapter.ProtectSocket(udpClient.Client);
+
+        return udpClient;
+    }
+
+    public void SetKeepAlive(Socket socket, bool enable)
+    {
+        _socketFactory.SetKeepAlive(socket, enable);
+    }
+}
