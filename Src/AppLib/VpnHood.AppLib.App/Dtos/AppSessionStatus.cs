5ff243227303823b1adea7c25d6fd0e6187f8bad
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:44:02 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Dtos/AppSessionStatus.cs 1752698592034

---
+++ Src/AppLib/VpnHood.AppLib.App/Dtos/AppSessionStatus.cs
@@ -0,0 +1,22 @@
+﻿using VpnHood.Core.Common.Messaging;
+
+namespace VpnHood.AppLib.Dtos;
+
+public class AppSessionStatus
+{
+    public required AppConnectorStat ConnectorStat { get; init; }
+    public required Traffic Speed { get; init; }
+    public required Traffic SessionTraffic { get; init; }
+    public required Traffic SessionSplitTraffic { get; init; }
+    public required Traffic CycleTraffic { get; init; }
+    public required Traffic TotalTraffic { get; init; }
+    public required int TcpTunnelledCount { get; init; }
+    public required int TcpPassthruCount { get; init; }
+    public required int PacketChannelCount { get; init; }
+    public required bool IsUdpMode { get; init; }
+    public required bool IsWaitingForAd { get; init; }
+    public required bool CanExtendByRewardedAd { get; init; }
+    public required long SessionMaxTraffic { get; init; }
+    public required DateTime? SessionExpirationTime { get; init; }
+    public required int? ActiveClientCount { get; init; }
+}
