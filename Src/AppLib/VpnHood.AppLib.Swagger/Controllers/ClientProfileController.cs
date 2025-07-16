10f81eb9ae36e331e01675ba80a6301be882cebc
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:42:43 +0200

    Add Src/AppLib/VpnHood.AppLib.Swagger/Controllers/ClientProfileController.cs 1752698513646

---
+++ Src/AppLib/VpnHood.AppLib.Swagger/Controllers/ClientProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VpnHood.AppLib.ClientProfiles;
+using VpnHood.AppLib.Swagger.Exceptions;
+using VpnHood.AppLib.WebServer.Api;
+
+namespace VpnHood.AppLib.Swagger.Controllers;
+
+[ApiController]
+[Route("api/client-profiles")]
+public class ClientProfileController : ControllerBase, IClientProfileController
+{
+    [HttpPut("access-keys")]
+    public Task<ClientProfileInfo> AddByAccessKey(string accessKey)
+    {
+        throw new SwaggerOnlyException();
+    }
+
+    [HttpGet("{clientProfileId}")]
+    public Task<ClientProfileInfo> Get(Guid clientProfileId)
+    {
+        throw new SwaggerOnlyException();
+    }
+
+    [HttpPatch("{clientProfileId}")]
+    public Task<ClientProfileInfo> Update(Guid clientProfileId, ClientProfileUpdateParams updateParams)
+    {
+        throw new SwaggerOnlyException();
+    }
+
+    [HttpDelete("{clientProfileId}")]
+    public Task Delete(Guid clientProfileId)
+    {
+        throw new SwaggerOnlyException();
+    }
+}
