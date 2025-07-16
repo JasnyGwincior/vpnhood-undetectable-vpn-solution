4187f94273a9bf04a4fd655a89a14906e94b8044
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:05 +0200

    Add Src/Core/VpnHood.Core.Client/ISessionStatus.cs 1752698355415

---
+++ Src/Core/VpnHood.Core.Client/ISessionStatus.cs
@@ -0,0 +1,25 @@
+﻿using VpnHood.Core.Client.Abstractions;
+using VpnHood.Core.Client.ConnectorServices;
+using VpnHood.Core.Common.Messaging;
+
+namespace VpnHood.Core.Client;
+
+public interface ISessionStatus
+{
+    ClientConnectorStat ConnectorStat { get; }
+    Traffic Speed { get; }
+    Traffic SessionTraffic { get; }
+    Traffic SessionSplitTraffic { get; }
+    Traffic CycleTraffic { get; }
+    Traffic TotalTraffic { get; }
+    int TcpTunnelledCount { get; }
+    int TcpPassthruCount { get; }
+    int SessionPacketChannelCount { get; }
+    int ActivePacketChannelCount { get; }
+    bool IsUdpMode { get; }
+    bool CanExtendByRewardedAd { get; }
+    long SessionMaxTraffic { get; }
+    DateTime? SessionExpirationTime { get; }
+    int? ActiveClientCount { get; }
+    AdRequest? AdRequest { get; }
+}
