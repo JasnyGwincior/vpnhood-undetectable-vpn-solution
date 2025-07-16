64a993d2862cd8996134bd449e517b055afa4e51
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:02 +0200

    Add Src/AppLib/VpnHood.AppLib.WebServer/Api/IAppController.cs 1752698532376

---
+++ Src/AppLib/VpnHood.AppLib.WebServer/Api/IAppController.cs
@@ -0,0 +1,27 @@
+﻿using VpnHood.AppLib.Settings;
+using VpnHood.Core.Client.Device;
+using VpnHood.Core.Common.Tokens;
+
+namespace VpnHood.AppLib.WebServer.Api;
+
+public interface IAppController
+{
+    Task<AppData> Configure(ConfigParams configParams);
+    Task<AppData> GetConfig();
+    Task<IpFilters> GetIpFilters();
+    Task SetIpFilters(IpFilters ipFilters);
+    Task<AppState> GetState();
+    Task Connect(Guid? clientProfileId, string? serverLocation, ConnectPlanId planId);
+    Task Diagnose(Guid? clientProfileId, string? serverLocation, ConnectPlanId planId);
+    Task Disconnect();
+    Task ClearLastError();
+    Task SetUserSettings(UserSettings userSettings);
+    Task<string> Log();
+    Task<DeviceAppInfo[]> GetInstalledApps();
+    Task VersionCheck();
+    Task VersionCheckPostpone();
+    Task OpenAlwaysOnPage();
+    Task RequestQuickLaunch();
+    Task RequestNotification();
+    Task ExtendByRewardedAd();
+}
