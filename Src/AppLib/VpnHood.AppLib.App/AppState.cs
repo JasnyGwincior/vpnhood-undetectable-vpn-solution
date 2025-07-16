a5af6c6dd68e1bc430c3609f7a686c6f866e50f7
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:44:06 +0200

    Add Src/AppLib/VpnHood.AppLib.App/AppState.cs 1752698595993

---
+++ Src/AppLib/VpnHood.AppLib.App/AppState.cs
@@ -0,0 +1,33 @@
+﻿using VpnHood.AppLib.Abstractions;
+using VpnHood.AppLib.ClientProfiles;
+using VpnHood.AppLib.Dtos;
+using VpnHood.Core.Toolkit.ApiClients;
+
+namespace VpnHood.AppLib;
+
+public class AppState
+{
+    public required DateTime ConfigTime { get; init; }
+    public required AppConnectionState ConnectionState { get; init; }
+    public required AppSessionInfo? SessionInfo { get; init; }
+    public required AppSessionStatus? SessionStatus { get; init; }
+    public required DateTime? ConnectRequestTime { get; init; }
+    public required ApiError? LastError { get; init; }
+    public required ClientProfileBaseInfo? ClientProfile { get; init; }
+    public required bool IsIdle { get; init; }
+    public required bool PromptForLog { get; init; }
+    public required bool LogExists { get; init; }
+    public required bool HasDiagnoseRequested { get; init; }
+    public required bool HasDisconnectedByUser { get; init; }
+    public required string? ClientCountryCode { get; init; }
+    public required string? ClientCountryName { get; init; }
+    public required VersionStatus VersionStatus { get; init; }
+    public required PublishInfo? LastPublishInfo { get; init; }
+    public required bool CanDisconnect { get; init; }
+    public required bool CanConnect { get; init; }
+    public required bool CanDiagnose { get; init; }
+    public required UiCultureInfo CurrentUiCultureInfo { get; init; }
+    public required UiCultureInfo SystemUiCultureInfo { get; init; }
+    public required BillingPurchaseState? PurchaseState { get; init; }
+    public required SystemBarsInfo SystemBarsInfo { get; init; }
+}
