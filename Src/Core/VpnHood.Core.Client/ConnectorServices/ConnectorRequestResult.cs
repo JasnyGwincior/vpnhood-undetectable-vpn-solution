e25aee1d7df9c01998f3a71eebe9255b377d09b3
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:44 +0200

    Add Src/Core/VpnHood.Core.Client/ConnectorServices/ConnectorRequestResult.cs 1752698394886

---
+++ Src/Core/VpnHood.Core.Client/ConnectorServices/ConnectorRequestResult.cs
@@ -0,0 +1,15 @@
+﻿using VpnHood.Core.Common.Messaging;
+using VpnHood.Core.Tunneling.ClientStreams;
+
+namespace VpnHood.Core.Client.ConnectorServices;
+
+internal class ConnectorRequestResult<T> : IDisposable where T : SessionResponse
+{
+    public required IClientStream ClientStream { get; init; }
+    public required T Response { get; init; }
+
+    public void Dispose()
+    {
+        ClientStream.Dispose();
+    }
+}
