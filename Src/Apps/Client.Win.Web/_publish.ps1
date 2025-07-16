19cdb64452e26af93519bdf05b3e7acfa2705294
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:46:55 +0200

    Add Src/Apps/Client.Win.Web/_publish.ps1 1752698765544

---
+++ Src/Apps/Client.Win.Web/_publish.ps1
@@ -0,0 +1,8 @@
+$SolutionDir = Split-Path -Parent -Path (Split-Path -Parent -Path (Split-Path -Parent -Path $PSScriptRoot));
+& "$SolutionDir/Pub/Core/PublishWinApp.ps1" `
+	-projectDir $PSScriptRoot  `
+	-packageFileTitle "VpnHoodClient" `
+	-aipFileR "Src/Apps/Client.Win.Web.Setup/VpnHood.App.Client.Win.Web.Setup.aip" `
+	-distribution "web" `
+	-repoUrl "https://github.com/vpnhood/VpnHood" `
+	-installationPageUrl "https://github.com/vpnhood/VpnHood/wiki/Install-VpnHood-Client"
