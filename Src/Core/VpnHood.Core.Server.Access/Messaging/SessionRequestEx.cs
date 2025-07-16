bf70ce1a9d7455ba7383bc7b1fa967794852d446
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:39:04 +0200

    Add Src/Core/VpnHood.Core.Server.Access/Messaging/SessionRequestEx.cs 1752698294378

---
+++ Src/Core/VpnHood.Core.Server.Access/Messaging/SessionRequestEx.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Core.Common.Messaging;
+using VpnHood.Core.Common.Tokens;
+using VpnHood.Core.Toolkit.Converters;
+
+namespace VpnHood.Core.Server.Access.Messaging;
+
+public class SessionRequestEx
+{
+    public required string TokenId { get; init; }
+    public required ClientInfo ClientInfo { get; init; }
+    public required byte[] EncryptedClientId { get; init; }
+    public string? ClientCountry { get; set; }
+    public required string? ExtraData { get; set; }
+    public string? ServerLocation { get; set; }
+    public ConnectPlanId PlanId { get; set; }
+    public bool AllowRedirect { get; set; } = true;
+    public bool? IsIpV6Supported { get; set; }
+    public int ProtocolVersion { get; set; }
+    public string? AccessCode { get; set; }
+
+    [JsonConverter(typeof(IPEndPointConverter))]
+    public required IPEndPoint HostEndPoint { get; set; }
+
+    [JsonConverter(typeof(IPAddressConverter))]
+    public required IPAddress? ClientIp { get; set; }
+}
