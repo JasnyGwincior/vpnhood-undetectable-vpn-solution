ae31b292af958326c6b1bde8bb24864b2daf1f8e
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:11:44 +0200

    Add Tests/VpnHood.Test/VpnHoodClientExtensions.cs 1752696654509

---
+++ Tests/VpnHood.Test/VpnHoodClientExtensions.cs
@@ -0,0 +1,39 @@
+﻿using VpnHood.Core.Client;
+using VpnHood.Core.Client.Abstractions;
+using VpnHood.Core.Common.Exceptions;
+using VpnHood.Core.Common.Messaging;
+using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.Test;
+
+public static class VpnHoodClientExtensions
+{
+    public static ISessionStatus GetSessionStatus(this VpnHoodClient client)
+        => client.SessionStatus ??
+           throw new InvalidOperationException("Session has not been initialized yet.");
+
+    public static SessionErrorCode GetLastSessionErrorCode(this VpnHoodClient client)
+        => (client.LastException as SessionException)?.SessionResponse.ErrorCode ?? SessionErrorCode.Ok;
+
+    public static Task WaitForState(this VpnHoodClient client, ClientState clientState, int timeout = 6000,
+        bool useUpdateStatus = false)
+    {
+        return VhTestUtil.AssertEqualsWait(clientState,
+            async () => {
+                if (useUpdateStatus)
+                    try {
+                        using var timeoutCts = new CancellationTokenSource(timeout);
+                        await client.UpdateSessionStatus(timeoutCts.Token);
+                    }
+                    catch {
+                        /*ignore*/
+                    }
+
+                return client.State;
+            },
+            "Client state didn't reach the expected value.",
+            timeout);
+    }
+
+
+}
