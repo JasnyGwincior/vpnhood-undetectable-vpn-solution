8ae2c1ebd5a860ce1141d8831a53ffa15b5710d2
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:45:07 +0200

    Add Src/AppLib/VpnHood.AppLib.Abstractions/IAppUiProvider.cs 1752698657882

---
+++ Src/AppLib/VpnHood.AppLib.Abstractions/IAppUiProvider.cs
@@ -0,0 +1,17 @@
+﻿using VpnHood.Core.Client.Device.UiContexts;
+
+namespace VpnHood.AppLib.Abstractions;
+
+public interface IAppUiProvider
+{
+    bool IsQuickLaunchSupported { get; }
+    Task<bool> RequestQuickLaunch(IUiContext uiContext, CancellationToken cancellationToken);
+
+    bool IsNotificationSupported { get; }
+    Task<bool> RequestNotification(IUiContext uiContext, CancellationToken cancellationToken);
+
+    bool IsOpenAlwaysOnPageSupported { get; }
+    void OpenAlwaysOnPage(IUiContext uiContext);
+    
+    SystemBarsInfo GetSystemBarsInfo(IUiContext uiContext);
+}
