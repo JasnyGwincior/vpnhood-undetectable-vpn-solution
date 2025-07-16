d87487e13d3d7f439103ef92bc5f343a6071e00e
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:37:18 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Utils/AsyncStreamLogger.cs 1752698188441

---
+++ Src/Core/VpnHood.Core.Tunneling/Utils/AsyncStreamLogger.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using VpnHood.Core.Toolkit.Logging;
+using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.Core.Tunneling.Utils;
+
+// Use for debugging
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public class AsyncStreamTracker(Stream sourceStream, bool leaveOpen)
+    : AsyncStreamDecorator(sourceStream, leaveOpen)
+{
+    public string LogPrefix { get; set; } = "";
+
+    public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        if (VhLogger.MinLogLevel == LogLevel.Trace)
+            VhLogger.Instance.LogTrace(LogPrefix + "Reading count. " + buffer.Length);
+        return base.ReadAsync(buffer, cancellationToken);
+    }
+
+    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        if (VhLogger.MinLogLevel == LogLevel.Trace)
+            VhLogger.Instance.LogTrace(LogPrefix + "writing count. " + buffer.Length);
+        return base.WriteAsync(buffer, cancellationToken);
+    }
+}
