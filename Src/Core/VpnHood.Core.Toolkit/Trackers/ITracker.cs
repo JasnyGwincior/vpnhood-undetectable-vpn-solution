b5c819978b910f1c248f589bad18ee6c3a44b90f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:22:36 +0200

    Add Src/Core/VpnHood.Core.Toolkit/Trackers/ITracker.cs 1752697306711

---
+++ Src/Core/VpnHood.Core.Toolkit/Trackers/ITracker.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Ga4.Trackers;
+
+public interface ITracker
+{
+    bool IsEnabled { get; set; }
+    Task Track(IEnumerable<TrackEvent> trackEvents, CancellationToken cancellationToken = default);
+    Task Track(TrackEvent trackEvent, CancellationToken cancellationToken = default);
+}
