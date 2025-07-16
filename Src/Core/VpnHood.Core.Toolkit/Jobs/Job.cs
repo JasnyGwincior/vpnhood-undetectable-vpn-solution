6f6cbe93cf23003091da9549d955ba69fd5f1e15
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:33:01 +0200

    Add Src/Core/VpnHood.Core.Toolkit/Jobs/Job.cs 1752697931760

---
+++ Src/Core/VpnHood.Core.Toolkit/Jobs/Job.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Extensions.Logging;
+using VpnHood.Core.Toolkit.Logging;
+using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.Core.Toolkit.Jobs;
+
+public class Job : IDisposable
+{
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    private readonly SemaphoreSlim _jobSemaphore = new(1, 1);
+    private readonly Func<CancellationToken, ValueTask> _jobFunc;
+    private readonly TimeSpan _dueTime;
+    private readonly int? _maxRetry;
+    private long _currentFailedCount;
+    private int _isDisposed;
+    private readonly JobRunner _jobRunner;
+    public TimeSpan Period { get; set; }
+    public long SucceededCount { get; private set; }
+    public long FailedCount { get; private set; }
+    public bool IsStarted => StartedTime != null;
+    public DateTime? StartedTime { get; set; }
+    public DateTime? LastExecutedTime { get; private set; }
+    public string Name { get; init; }
+
+    public Job(Func<CancellationToken, ValueTask> jobFunc, JobOptions options)
+    {
+        _jobFunc = jobFunc;
+        _dueTime = options.DueTime ?? options.Period;
+        Period = options.Period;
+        _maxRetry = options.MaxRetry;
+        Name = options.Name ?? "NoName";
+        if (options.AutoStart)
+            Start();
+
+        // initialize job runner based on the period
+        _jobRunner = options.Period >= JobRunner.SlowInstance.Interval
+            ? JobRunner.SlowInstance
+            : JobRunner.FastInstance;
+
+        _jobRunner.Add(this);
+    }
+
+    public Job(Func<CancellationToken, ValueTask> jobFunc, TimeSpan period, string? name = null)
+        : this(jobFunc, new JobOptions {
+            Period = period,
+            Name = name,
+            DueTime = TimeSpan.Zero,
+        })
+    {
+    }
+    public Job(Func<CancellationToken, ValueTask> jobFunc, string? name = null)
+        : this(jobFunc, new JobOptions {
+            Name = name,
+            DueTime = TimeSpan.Zero,
+        })
+    {
+    }
+
+    public void Start()
+    {
+        if (_isDisposed != 0)
+            throw new ObjectDisposedException(nameof(Job));
+
+        if (IsStarted)
+            throw new InvalidOperationException("Job is already started.");
+
+        StartedTime = FastDateTime.Now;
+    }
+
+    public void Stop()
+    {
+        if (!IsStarted)
+            throw new InvalidOperationException("Job is not started.");
+
+        StartedTime = null;
+        _currentFailedCount = 0;
+    }
+
+    public bool IsReadyToRun {
+        get {
+            // job is not started
+            if (StartedTime is null)
+                return false;
+
+            // Someone else is currently running the job (semaphore is taken)
+            if (_jobSemaphore.CurrentCount == 0)
+                return false;
+
+            var now = FastDateTime.Now;
+
+            // first time execution after due time 
+            if (LastExecutedTime is null)
+                return now - StartedTime >= _dueTime;
+
+            // interval execution
+            return now - LastExecutedTime >= Period;
+        }
+    }
+
+    public Task RunNow()
+    {
+        return RunInternal(_cancellationTokenSource.Token);
+    }
+
+    public async Task RunNow(CancellationToken cancellationToken)
+    {
+        using var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cancellationTokenSource.Token);
+
+        // await required for linkedCts to be disposed properly
+        await RunInternal(linkedCts.Token);
+    }
+
+    private async Task RunInternal(CancellationToken cancellationToken)
+    {
+        if (_isDisposed != 0)
+            throw new ObjectDisposedException(nameof(Job));
+
+        try {
+            await _jobSemaphore.WaitAsync(cancellationToken).Vhc();
+            await _jobFunc(cancellationToken).Vhc();
+            _currentFailedCount = 0;
+            SucceededCount++;
+        }
+        catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested) {
+            // job was cancelled and no logging is needed
+        }
+        catch (Exception ex) {
+            FailedCount++;
+            _currentFailedCount++;
+
+            // stop the job if it has failed too many times
+            if (_currentFailedCount > _maxRetry) {
+                VhLogger.Instance.LogError(ex,
+                    "Job failed too many times and stopped. JobName: {JobName}, FailedCount: {FailedCount}, TotalErrorCount: {TotalFailedCount}",
+                    Name, _currentFailedCount, FailedCount);
+
+                Stop();
+            }
+
+            throw;
+        }
+        finally {
+            LastExecutedTime = FastDateTime.Now;
+            _jobSemaphore.Release();
+        }
+    }
+
+    public void Dispose()
+    {
+        if (Interlocked.Exchange(ref _isDisposed, 1) == 1)
+            return;
+
+        _cancellationTokenSource.TryCancel();
+        _cancellationTokenSource.Dispose();
+        _jobSemaphore.Dispose();
+        StartedTime = null;
+        _jobRunner.Remove(this);
+    }
+}
