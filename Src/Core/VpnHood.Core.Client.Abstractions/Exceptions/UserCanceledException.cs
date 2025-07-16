1c1d8be763010af9b85b3fcb155e861629c67fac
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:56 +0200

    Add Src/Core/VpnHood.Core.Client.Abstractions/Exceptions/UserCanceledException.cs 1752698466809

---
+++ Src/Core/VpnHood.Core.Client.Abstractions/Exceptions/UserCanceledException.cs
@@ -0,0 +1,13 @@
+﻿namespace VpnHood.Core.Client.Abstractions.Exceptions;
+
+public class UserCanceledException : OperationCanceledException
+{
+    public UserCanceledException(string message) : base(message)
+    {
+
+    }
+
+    public UserCanceledException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
