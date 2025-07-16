1d08f02a4adefc15e3a7c7ac47fa34fc6147bf91
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:33:31 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Sockets/ISocketFactory.cs 1752697961532

---
+++ Src/Core/VpnHood.Core.Tunneling/Sockets/ISocketFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace VpnHood.Core.Tunneling.Sockets;
+
+public interface ISocketFactory
+{
+    public TcpClient CreateTcpClient(IPEndPoint ipEndPoint);
+    public UdpClient CreateUdpClient(AddressFamily addressFamily);
+    public void SetKeepAlive(Socket socket, bool enable);
+}
