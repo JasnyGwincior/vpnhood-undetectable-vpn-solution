b228f4c23e3dbd91fc8728f38d462671bfe50f55
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:41:57 +0200

    Add Src/Core/VpnHood.Core.Client.Abstractions/Exceptions/UnreachableServerException.cs 1752698467796

---
+++ Src/Core/VpnHood.Core.Client.Abstractions/Exceptions/UnreachableServerException.cs
@@ -0,0 +1,14 @@
+﻿namespace VpnHood.Core.Client.Abstractions.Exceptions;
+
+public class UnreachableServerException : Exception
+{
+    public UnreachableServerException(string message)
+        : base(message)
+    {
+    }
+
+    public UnreachableServerException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
