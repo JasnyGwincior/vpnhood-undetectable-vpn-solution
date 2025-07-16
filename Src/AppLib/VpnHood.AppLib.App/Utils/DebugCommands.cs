f097cecd1041f7258470c20108eda56b76f3397f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:15 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Utils/DebugCommands.cs 1752698545027

---
+++ Src/AppLib/VpnHood.AppLib.App/Utils/DebugCommands.cs
@@ -0,0 +1,26 @@
+﻿namespace VpnHood.AppLib.Utils;
+
+public static class DebugCommands
+{
+    public const string CaptureContext = "/capture-context";
+    public const string DropUdp = "/drop-udp";
+    public const string KillSpaServer = "/kill-spa-server";
+    public const string UseTcpOverTun = "/tcp-over-tun";
+    public const string LogDebug = "/log:debug";
+    public const string LogTrace = "/log:trace";
+    public const string NullCapture = "/null-capture";
+    public const string NoTcpReuse = "/no-tcp-reuse";
+    public const string WinDivert = "/windivert";
+
+    public static string[] All => [
+        CaptureContext,
+        DropUdp,
+        LogDebug,
+        LogTrace,
+        KillSpaServer,
+        UseTcpOverTun,
+        NoTcpReuse,
+        NullCapture,
+        WinDivert
+    ];
+}
