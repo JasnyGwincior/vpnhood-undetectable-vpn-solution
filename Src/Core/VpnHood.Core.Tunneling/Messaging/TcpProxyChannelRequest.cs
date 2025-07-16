362727d2204e467249caa55ac084c488db706217
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:37:30 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Messaging/TcpProxyChannelRequest.cs 1752698199997

---
+++ Src/Core/VpnHood.Core.Tunneling/Messaging/TcpProxyChannelRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Core.Toolkit.Converters;
+
+namespace VpnHood.Core.Tunneling.Messaging;
+
+public class StreamProxyChannelRequest()
+    : RequestBase(Messaging.RequestCode.ProxyChannel)
+{
+    [JsonConverter(typeof(IPEndPointConverter))]
+    public required IPEndPoint DestinationEndPoint { get; set; }
+
+    [Obsolete("Will be removed after 688. Required for server compatibility smaller than 688.")]
+    public byte[] CipherKey { get; init; } = [];
+
+    [Obsolete("Will be removed after 688. Required for server compatibility smaller than 688.")]
+    public long CipherLength { get; init; } = 0;
+}
