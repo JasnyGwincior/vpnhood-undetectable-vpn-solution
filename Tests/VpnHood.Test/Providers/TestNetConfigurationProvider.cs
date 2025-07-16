a8616018fe77eecf1702f0756e5a28d65c3da643
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:12:14 +0200

    Add Tests/VpnHood.Test/Providers/TestNetConfigurationProvider.cs 1752696684338

---
+++ Tests/VpnHood.Test/Providers/TestNetConfigurationProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using VpnHood.Core.Server.Abstractions;
+
+namespace VpnHood.Test.Providers;
+
+public class TestNetConfigurationProvider : INetConfigurationProvider
+{
+    public ConcurrentDictionary<IPAddress, string> IpAddresses { get; } = new();
+
+    public Task<string[]> GetInterfaceNames()
+    {
+        return Task.FromResult(new[] { "eth0", "eth1" });
+    }
+
+    public Task AddIpAddress(IPAddress ipAddress, string interfaceName)
+    {
+        IpAddresses.TryAdd(ipAddress, interfaceName);
+        return Task.CompletedTask;
+    }
+
+    public Task RemoveIpAddress(IPAddress ipAddress, string interfaceName)
+    {
+        IpAddresses.TryRemove(ipAddress, out _);
+        return Task.CompletedTask;
+    }
+
+    public Task<bool> IpAddressExists(IPAddress ipAddress)
+    {
+        return Task.FromResult(IpAddresses.ContainsKey(ipAddress));
+    }
+
+    public Task<string> GetTcpCongestionControl()
+    {
+        throw new NotSupportedException();
+    }
+
+    public Task SetTcpCongestionControl(string value)
+    {
+        throw new NotSupportedException();
+    }
+}
