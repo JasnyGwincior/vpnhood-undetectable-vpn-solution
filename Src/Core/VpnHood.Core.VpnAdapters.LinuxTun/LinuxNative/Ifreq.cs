9729e0923ebf4cb70f3a4734d0165e9988b10388
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:17:33 +0200

    Add Src/Core/VpnHood.Core.VpnAdapters.LinuxTun/LinuxNative/Ifreq.cs 1752697003129

---
+++ Src/Core/VpnHood.Core.VpnAdapters.LinuxTun/LinuxNative/Ifreq.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace VpnHood.Core.VpnAdapters.LinuxTun.LinuxNative;
+
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+internal struct Ifreq
+{
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+    public string ifr_name; // Interface name
+    public short ifr_flags; // Flags (e.g., IFF_TUN)
+    
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    public byte[] padding;
+}
