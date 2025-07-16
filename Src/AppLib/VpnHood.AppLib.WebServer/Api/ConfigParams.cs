d3bbea05dcf3a2f66ed3446f99496d65679f3aef
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:04 +0200

    Add Src/AppLib/VpnHood.AppLib.WebServer/Api/ConfigParams.cs 1752698534313

---
+++ Src/AppLib/VpnHood.AppLib.WebServer/Api/ConfigParams.cs
@@ -0,0 +1,8 @@
+﻿using VpnHood.AppLib.Abstractions;
+namespace VpnHood.AppLib.WebServer.Api;
+
+public class ConfigParams
+{
+    public string[] AvailableCultures { get; init; } = [];
+    public AppResources.AppStrings? Strings { get; init; }
+}
