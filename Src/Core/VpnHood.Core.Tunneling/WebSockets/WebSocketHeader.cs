17e81e975bf809c968f7c4758a65959d52d2599c
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:21:45 +0200

    Add Src/Core/VpnHood.Core.Tunneling/WebSockets/WebSocketHeader.cs 1752697255343

---
+++ Src/Core/VpnHood.Core.Tunneling/WebSockets/WebSocketHeader.cs
@@ -0,0 +1,12 @@
+﻿namespace VpnHood.Core.Tunneling.WebSockets;
+
+public readonly struct WebSocketHeader
+{
+    public bool IsBinary { get; init; } 
+    public bool IsText { get; init; } 
+    public bool IsPing { get; init; } 
+    public bool IsPong { get; init; } 
+    public bool IsCloseConnection { get; init; } 
+    public required long PayloadLength { get; init; }
+    public ReadOnlyMemory<byte> MaskKey { get; init; }
+}
