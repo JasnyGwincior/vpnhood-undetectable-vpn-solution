e4d922647152c395f011face3db0569d0244cb45
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:44:03 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Dtos/AppSessionInfo.cs 1752698593001

---
+++ Src/AppLib/VpnHood.AppLib.App/Dtos/AppSessionInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Core.Common.Messaging;
+using VpnHood.Core.Common.Tokens;
+using VpnHood.Core.Toolkit.Converters;
+
+namespace VpnHood.AppLib.Dtos;
+
+public class AppSessionInfo
+{
+    public required AccessInfo? AccessInfo { get; init; }
+    public required bool IsUdpChannelSupported { get; init; }
+    public required bool IsDnsServersAccepted { get; init; }
+    public required ServerLocationInfo? ServerLocationInfo { get; init; }
+    public required bool IsPremiumSession { get; init; }
+    public required SessionSuppressType SuppressedTo { get; init; }
+
+    [JsonConverter(typeof(ArrayConverter<IPAddress, IPAddressConverter>))]
+    public required IPAddress[] DnsServers { get; init; }
+
+    [JsonConverter(typeof(VersionConverter))]
+    public required Version ServerVersion { get; init; }
+
+    [JsonConverter(typeof(IPAddressConverter))]
+    public required IPAddress ClientPublicIpAddress { get; init; }
+}
