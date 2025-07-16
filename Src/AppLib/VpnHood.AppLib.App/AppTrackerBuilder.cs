e4ee536832accee4ea0f0509c594aeada29ee1ee
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:44:05 +0200

    Add Src/AppLib/VpnHood.AppLib.App/AppTrackerBuilder.cs 1752698594963

---
+++ Src/AppLib/VpnHood.AppLib.App/AppTrackerBuilder.cs
@@ -0,0 +1,52 @@
+﻿using Ga4.Trackers;
+
+namespace VpnHood.AppLib;
+
+public static class AppTrackerBuilder
+{
+    public static TrackEvent BuildLoadAdFailed(string adNetwork, string errorMessage, string? countryCode)
+    {
+        return new TrackEvent {
+            EventName = "vh_ad_load_failed",
+            Parameters = new Dictionary<string, object> {
+                { "error", errorMessage },
+                { "ad_network", adNetwork },
+                { "country", countryCode ?? "(vh_unknown)" }
+            }
+        };
+    }
+
+    public static TrackEvent BuildShowAdFailed(string? adNetwork, string errorMessage, string? countryCode)
+    {
+        return new TrackEvent {
+            EventName = "vh_ad_show_failed",
+            Parameters = new Dictionary<string, object> {
+                { "ad_network", adNetwork ?? "(vh_unknown)" },
+                { "error", errorMessage },
+                { "country", countryCode ?? "(vh_unknown)" }
+            }
+        };
+    }
+
+    public static TrackEvent BuildShowAdOk(string adNetwork, string? countryCode)
+    {
+        return new TrackEvent {
+            EventName = "vh_ad_show_ok",
+            Parameters = new Dictionary<string, object> {
+                { "ad_network", adNetwork },
+                { "country", countryCode ?? "(vh_unknown)" },
+            }
+        };
+    }
+
+    public static TrackEvent BuildFirstLaunch(string deviceId, string countryCode)
+    {
+        return new TrackEvent {
+            EventName = "vh_first_launch",
+            Parameters = new Dictionary<string, object> {
+                { "deviceId", deviceId },
+                { "country", countryCode }
+            }
+        };
+    }
+}
