81780c686db9f1cf50284927b0907d60e4432247
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:24 +0200

    Add Src/Core/VpnHood.Core.Common/Messaging/AccessUsage.cs 1752698374374

---
+++ Src/Core/VpnHood.Core.Common/Messaging/AccessUsage.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.Core.Common.Messaging;
+
+public class AccessUsage
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool CanExtendByRewardedAd { get; set; }
+
+    public bool IsPremium { get; set; } // session premium
+    public Traffic CycleTraffic { get; set; } = new(); // total traffic
+    public Traffic TotalTraffic { get; set; } = new(); // total traffic
+    public long MaxTraffic { get; set; } // session max traffic
+    public DateTime? ExpirationTime { get; set; } // It is the session expiration not access expiration
+    public int? ActiveClientCount { get; set; }
+}
