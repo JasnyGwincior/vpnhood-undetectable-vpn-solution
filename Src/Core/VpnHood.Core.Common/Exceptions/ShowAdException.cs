cd89e107807267e04e92585d6420c1e93f0d28dc
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:48 +0200

    Add Src/Core/VpnHood.Core.Common/Exceptions/ShowAdException.cs 1752698398111

---
+++ Src/Core/VpnHood.Core.Common/Exceptions/ShowAdException.cs
@@ -0,0 +1,13 @@
+﻿namespace VpnHood.Core.Common.Exceptions;
+
+public class ShowAdException : AdException
+{
+    public string? AdNetworkName { get; init; }
+    public ShowAdException(string message) : base(message)
+    {
+    }
+
+    public ShowAdException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
