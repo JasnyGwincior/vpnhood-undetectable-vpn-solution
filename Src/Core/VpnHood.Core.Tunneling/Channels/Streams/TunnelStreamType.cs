48138ef039cf5b585017bb891c2cb803ce08dc7c
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:37:57 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Channels/Streams/TunnelStreamType.cs 1752698227104

---
+++ Src/Core/VpnHood.Core.Tunneling/Channels/Streams/TunnelStreamType.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VpnHood.Core.Tunneling.Channels.Streams;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum TunnelStreamType
+{
+    Unknown,
+    None,
+    Standard,
+    WebSocket,
+    WebSocketNoHandshake
+}
