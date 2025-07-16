52973970e8da691e574974f059b29ffab5d00f4f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:22:17 +0200

    Add Src/Core/VpnHood.Core.Toolkit/Utils/TaskExtensions.cs 1752697287224

---
+++ Src/Core/VpnHood.Core.Toolkit/Utils/TaskExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+using VpnHood.Core.Toolkit.Logging;
+
+namespace VpnHood.Core.Toolkit.Utils;
+
+public static class TaskExtensions
+{
+    public static bool DefaultContinueOnCapturedContext { get; set; }
+
+    public static ConfiguredTaskAwaitable Vhc(this Task task)
+    {
+        return task.ConfigureAwait(DefaultContinueOnCapturedContext);
+    }
+
+    public static ConfiguredTaskAwaitable<T> Vhc<T>(this Task<T> task)
+    {
+        return task.ConfigureAwait(DefaultContinueOnCapturedContext);
+    }
+
+    public static ConfiguredValueTaskAwaitable Vhc(this ValueTask task)
+    {
+        return task.ConfigureAwait(DefaultContinueOnCapturedContext);
+    }
+
+    public static ConfiguredValueTaskAwaitable<T> Vhc<T>(this ValueTask<T> task)
+    {
+        return task.ConfigureAwait(DefaultContinueOnCapturedContext);
+    }
+
+    public static void VhBlock(this ValueTask task)
+    {
+        if (!task.IsCompleted)
+            task.GetAwaiter().GetResult();
+    }
+
+    public static void TryCancel(this CancellationTokenSource cancellationTokenSource)
+    {
+        try {
+            if (!cancellationTokenSource.IsCancellationRequested)
+                cancellationTokenSource.Cancel();
+        }
+        catch (Exception ex) {
+            VhLogger.Instance.LogError(ex,
+                "Failed to cancel the CancellationTokenSource. This is not a critical error.");
+        }
+    }
+
+    public static async Task TryCancelAsync(this CancellationTokenSource cancellationTokenSource)
+    {
+        try {
+            if (!cancellationTokenSource.IsCancellationRequested)
+                await cancellationTokenSource.CancelAsync().Vhc();
+        }
+        catch (Exception ex) {
+            VhLogger.Instance.LogError(ex,
+                "Failed to cancel the CancellationTokenSource. This is not a critical error.");
+        }
+    }
+
+}
