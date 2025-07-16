e051abd83d9c6a09f366b50f53c68cc65eb92b68
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:23 +0200

    Add Src/Core/VpnHood.Core.Common/Messaging/ClientInfo.cs 1752698373250

---
+++ Src/Core/VpnHood.Core.Common/Messaging/ClientInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace VpnHood.Core.Common.Messaging;
+
+public class ClientInfo
+{
+    public required string ClientId { get; init; }
+    public required string ClientVersion { get; init; }
+    public int MinProtocolVersion { get; init; }
+    public int MaxProtocolVersion { get; init; }
+    public required string UserAgent { get; init; }
+
+    public override string ToString()
+    {
+        return $"{nameof(ClientId)}={ClientId}";
+    }
+}
