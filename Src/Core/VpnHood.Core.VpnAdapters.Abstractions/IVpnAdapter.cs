5a0c61b47da148767543371220e898ace8c46e30
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:17:43 +0200

    Add Src/Core/VpnHood.Core.VpnAdapters.Abstractions/IVpnAdapter.cs 1752697013713

---
+++ Src/Core/VpnHood.Core.VpnAdapters.Abstractions/IVpnAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using VpnHood.Core.Packets;
+using VpnHood.Core.PacketTransports;
+
+namespace VpnHood.Core.VpnAdapters.Abstractions;
+
+public interface IVpnAdapter : IPacketTransport
+{
+    event EventHandler? Disposed;
+    bool IsStarted { get; }
+    bool IsNatSupported { get; }
+    bool CanProtectSocket { get; }
+    bool ProtectSocket(Socket socket);
+    bool ProtectSocket(Socket socket, IPAddress ipAddress);
+    Task Start(VpnAdapterOptions options, CancellationToken cancellationToken);
+    void Stop();
+    IPAddress? GetPrimaryAdapterAddress(IpVersion ipVersion);
+    bool IsIpVersionSupported(IpVersion ipVersion);
+}
