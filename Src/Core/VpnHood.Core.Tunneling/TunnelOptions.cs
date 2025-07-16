120bd8acbb061fa6af60beea853688a2f869efb6
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:33:25 +0200

    Add Src/Core/VpnHood.Core.Tunneling/TunnelOptions.cs 1752697955754

---
+++ Src/Core/VpnHood.Core.Tunneling/TunnelOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace VpnHood.Core.Tunneling;
+
+public class TunnelOptions
+{
+    public required int MaxPacketChannelCount { get; init; } = 8;
+    public required int PacketQueueCapacity { get; init; }
+    public required bool AutoDisposePackets { get; init; }
+    public bool UseSpeedometerTimer { get; init; }
+}
