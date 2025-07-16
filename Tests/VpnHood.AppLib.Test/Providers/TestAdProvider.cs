5748bb2ffd71be0e6d267c5234966c9beaddf89f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:46:50 +0200

    Add Tests/VpnHood.AppLib.Test/Providers/TestAdProvider.cs 1752698760897

---
+++ Tests/VpnHood.AppLib.Test/Providers/TestAdProvider.cs
@@ -0,0 +1,49 @@
+﻿using VpnHood.AppLib.Abstractions;
+using VpnHood.Core.Client.Device.UiContexts;
+using VpnHood.Core.Common.Exceptions;
+using VpnHood.Test.AccessManagers;
+
+namespace VpnHood.AppLib.Test.Providers;
+
+public class TestAdProvider(TestAccessManager accessManager, AppAdType adType = AppAdType.RewardedAd) : IAppAdProvider
+{
+    public bool FailShow { get; set; }
+    public bool FailLoad { get; set; }
+    public string NetworkName => "UnitTestNetwork";
+    public AppAdType AdType => adType;
+    public DateTime? AdLoadedTime { get; private set; }
+    public TimeSpan AdLifeSpan { get; } = TimeSpan.FromMinutes(60);
+
+    public Task LoadAd(IUiContext uiContext, CancellationToken cancellationToken)
+    {
+        AdLoadedTime = null;
+        if (FailLoad)
+            throw new LoadAdException("Load Ad failed.");
+
+        AdLoadedTime = DateTime.Now;
+        return Task.CompletedTask;
+    }
+
+    public Task ShowAd(IUiContext uiContext, string? customData, CancellationToken cancellationToken)
+    {
+        if (AdLoadedTime == null)
+            throw new ShowAdException("Not Ad has been loaded.");
+
+        try {
+            if (FailShow)
+                throw new ShowAdException("Ad failed.");
+
+            if (!string.IsNullOrEmpty(customData))
+                accessManager.AddAdData(customData);
+
+            return Task.CompletedTask;
+        }
+        finally {
+            AdLoadedTime = null;
+        }
+    }
+
+    public void Dispose()
+    {
+    }
+}
