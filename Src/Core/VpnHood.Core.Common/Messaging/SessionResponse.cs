0f521735ab10589877c2873eac4762966eb104cb
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:40:32 +0200

    Add Src/Core/VpnHood.Core.Common/Messaging/SessionResponse.cs 1752698382064

---
+++ Src/Core/VpnHood.Core.Common/Messaging/SessionResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Core.Toolkit.Converters;
+
+namespace VpnHood.Core.Common.Messaging;
+
+public class SessionResponse
+{
+    public required SessionErrorCode ErrorCode { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? ErrorMessage { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public AccessUsage? AccessUsage { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public SessionSuppressType SuppressedBy { get; set; }
+
+    [JsonConverter(typeof(IPEndPointConverter))]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public IPEndPoint? RedirectHostEndPoint { get; set; }
+
+    [JsonConverter(typeof(ArrayConverter<IPEndPoint, IPEndPointConverter>))]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public IPEndPoint[]? RedirectHostEndPoints { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? AccessKey { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string? ClientCountry { get; set; }
+    
+    [JsonConverter(typeof(IPAddressConverter))]
+    public IPAddress? ClientPublicAddress { get; set; }
+}
