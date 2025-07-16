dc6a83926b77803118cda7a021ad4915fbde4a22
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:42:04 +0200

    Add Src/AppLib/VpnHood.AppLib.Win.Common.WpfSpa/VpnHoodWpfSpaApp.cs 1752698474549

---
+++ Src/AppLib/VpnHood.AppLib.Win.Common.WpfSpa/VpnHoodWpfSpaApp.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Net;
+using System.Security.Principal;
+using System.Windows;
+using Microsoft.Extensions.Logging;
+using VpnHood.AppLib.WebServer;
+using VpnHood.Core.Client.Device.Win;
+using VpnHood.Core.Toolkit.Logging;
+
+namespace VpnHood.AppLib.Win.Common.WpfSpa;
+
+// ReSharper disable once RedundantExtendsListEntry
+public abstract class VpnHoodWpfSpaApp : Application
+{
+    protected abstract AppOptions CreateAppOptions();
+    public abstract bool SpaListenToAllIps { get; }
+    public abstract int? SpaDefaultPort { get; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        try {
+            // initialize Win App
+            var appOptions = CreateAppOptions();
+            appOptions.DeviceId ??= WindowsIdentity.GetCurrent().User?.Value;
+            appOptions.EventWatcherInterval ??= TimeSpan.FromSeconds(1);
+
+            // initialize VpnHoodWinApp
+            VpnHoodWinApp.Init(appId: appOptions.AppId, storageFolder: appOptions.StorageFolderPath, args: e.Args);
+
+            // initialize VpnHoodApp
+            VpnHoodApp.Init(new WinDevice(appOptions.StorageFolderPath, appOptions.IsDebugMode), appOptions);
+
+            // initialize SPA
+            ArgumentNullException.ThrowIfNull(VpnHoodApp.Instance.Resources.SpaZipData);
+            using var spaResource = new MemoryStream(VpnHoodApp.Instance.Resources.SpaZipData);
+            var localSpaUrl = !string.IsNullOrEmpty(appOptions.LocalSpaHostName)
+                ? VpnHoodWinApp.RegisterLocalDomain(new IPEndPoint(IPAddress.Parse("127.10.10.10"), SpaDefaultPort ?? 80), appOptions.LocalSpaHostName)
+                : null;
+
+            VpnHoodAppWebServer.Init(new WebServerOptions {
+                SpaZipStream = spaResource,
+                DefaultPort = SpaDefaultPort,
+                ListenOnAllIps = SpaListenToAllIps,
+                Url = localSpaUrl
+            });
+
+            // initialize Win
+            VpnHoodWinApp.Instance.ExitRequested += (_, _) => Shutdown();
+            VpnHoodWinApp.Instance.OpenMainWindowInBrowserRequested += (_, _) =>
+                VpnHoodWinApp.OpenUrlInExternalBrowser(VpnHoodAppWebServer.Instance.Url);
+            VpnHoodWinApp.Instance.OpenMainWindowRequested += OpenMainWindowRequested;
+            VpnHoodWinApp.Instance.Start();
+
+            // run the app
+            var mainWindow = new VpnHoodWpfSpaMainWindow();
+            mainWindow.Show();
+        }
+        catch (Exception ex) {
+            VhLogger.Instance.LogError(ex, "Could not run the app.");
+            Shutdown();
+            throw;
+        }
+    }
+
+    private void OpenMainWindowRequested(object? sender, EventArgs e)
+    {
+        Dispatcher.Invoke(() => {
+            if (MainWindow == null)
+                return;
+
+            MainWindow.Show();
+            MainWindow.Activate();
+            MainWindow.WindowState = WindowState.Normal;
+        });
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        VpnHoodWinApp.Instance.Dispose();
+        if (VpnHoodAppWebServer.IsInit) VpnHoodAppWebServer.Instance.Dispose();
+        if (VpnHoodApp.IsInit) VpnHoodApp.Instance.DisposeAsync().AsTask().Wait(TimeSpan.FromSeconds(5));
+
+        base.OnExit(e);
+    }
+}
