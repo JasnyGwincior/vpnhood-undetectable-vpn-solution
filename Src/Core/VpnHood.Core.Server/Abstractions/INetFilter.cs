3ef1644be9d47edbe4d6459c0067e14ada83d0f4
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:38:43 +0200

    Add Src/Core/VpnHood.Core.Server/Abstractions/INetFilter.cs 1752698273210

---
+++ Src/Core/VpnHood.Core.Server/Abstractions/INetFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using VpnHood.Core.Packets;
+using VpnHood.Core.Toolkit.Net;
+
+namespace VpnHood.Core.Server.Abstractions;
+
+public interface INetFilter
+{
+    public IpRangeOrderedList BlockedIpRanges { get; set; }
+    public IpPacket? ProcessRequest(IpPacket ipPacket);
+    public IPEndPoint? ProcessRequest(IpProtocol protocol, IPEndPoint requestEndPoint);
+    public IpPacket ProcessReply(IpPacket ipPacket);
+}
