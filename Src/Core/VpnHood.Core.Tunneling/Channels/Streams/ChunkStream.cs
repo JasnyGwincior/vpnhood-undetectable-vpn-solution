809d7a01422f8a74d643b5fa65d0f4b2334f204f
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:38:00 +0200

    Add Src/Core/VpnHood.Core.Tunneling/Channels/Streams/ChunkStream.cs 1752698230368

---
+++ Src/Core/VpnHood.Core.Tunneling/Channels/Streams/ChunkStream.cs
@@ -0,0 +1,31 @@
+﻿using VpnHood.Core.Toolkit.Utils;
+
+namespace VpnHood.Core.Tunneling.Channels.Streams;
+
+public abstract class ChunkStream : AsyncStreamDecorator
+{
+    protected bool AllowReuse = true;
+
+    public abstract bool CanReuse { get; }
+    public abstract Task<ChunkStream> CreateReuse();
+    public void PreventReuse() => AllowReuse = false;
+    public int ReusedCount { get; }
+    public int ReadChunkCount { get; protected set; }
+    public int WroteChunkCount { get; protected set; }
+    public string StreamId { get; internal set; }
+
+
+    protected ChunkStream(Stream sourceStream, string streamId)
+        : base(sourceStream, leaveOpen: true)
+    {
+        StreamId = streamId;
+        ReusedCount = 0;
+    }
+
+    protected ChunkStream(Stream sourceStream, string streamId, int reusedCount)
+        : base(sourceStream, true)
+    {
+        StreamId = streamId;
+        ReusedCount = reusedCount;
+    }
+}
