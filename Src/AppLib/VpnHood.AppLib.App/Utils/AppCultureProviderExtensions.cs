36da105568ae23ef4ee9f9ecc4d0cbd55823bc53
JasnyGwincior <bermudadragan11@gmail.com>
2025-07-16 22:43:16 +0200

    Add Src/AppLib/VpnHood.AppLib.App/Utils/AppCultureProviderExtensions.cs 1752698546043

---
+++ Src/AppLib/VpnHood.AppLib.App/Utils/AppCultureProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using VpnHood.AppLib.Abstractions;
+
+namespace VpnHood.AppLib.Utils;
+
+public static class AppCultureProviderExtensions
+{
+    public static CultureInfo GetBestCultureInfo(this IAppCultureProvider cultureProvider)
+    {
+        var availableCultures = cultureProvider.AvailableCultures;
+        var systemCulture =
+            new CultureInfo(cultureProvider.SystemCultures.FirstOrDefault() ?? CultureInfo.InstalledUICulture.Name);
+        var culture = systemCulture;
+        while (culture.Name != string.Empty) {
+            if (availableCultures.Contains(culture.Name, StringComparer.CurrentCultureIgnoreCase))
+                return culture;
+
+            culture = culture.Parent;
+        }
+
+        return systemCulture;
+    }
+}
